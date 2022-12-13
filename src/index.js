a838ebd245afe9fe0229ed2ba0574db82b7c2964
Niko123454321 <kvbsnsk@gmail.com>
2022-12-13 23:12:00 +0200

    axios & async/await

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from 'axios';
 
 const refs = {
   searchForm: document.querySelector('#search-form'),
@@ -59,27 +60,25 @@ function clear() {
   refs.gallery.innerHTML = '';
 }
 
-function onFetch() {
-  fetch(
-    `${BASE_URL}/${options.key}&${options.orientation}&${options.q}&${options.safesearch}&${options.image_type}&${options.perPage}&page=${page}`
-  )
-    .then(res => {
-      return res.json();
-    })
-    .then(obj => {
-      items = obj;
+async function onFetch() {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/${options.key}&${options.orientation}&${options.q}&${options.safesearch}&${options.image_type}&${options.perPage}&page=${page}`
+    );
 
-      console.log(`totalHits: ${items.totalHits}`);
-      console.log('+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+');
+    items = response.data;
 
-      if (items.total === 0) {
-        return notifyFail();
-      } else {
-        notifySuccess();
-        items.hits.forEach(element => {
-          refs.gallery.insertAdjacentHTML(
-            'beforeend',
-            `<div class="photo-card">
+    console.log(`totalHits: ${items.totalHits}`);
+    console.log('+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+');
+
+    if (items.total === 0) {
+      return notifyFail();
+    } else {
+      notifySuccess();
+      items.hits.forEach(element => {
+        refs.gallery.insertAdjacentHTML(
+          'beforeend',
+          `<div class="photo-card">
           <a href="${element.webformatURL}" >
     <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" /></a>
     <div class="info">
@@ -97,16 +96,67 @@ function onFetch() {
       </p>
     </div>
   </div>`
-          );
-        });
-        var lightbox = new SimpleLightbox('.gallery a', {
-          /* options */
-        });
-        lightbox.refresh();
-        refs.button_load.classList.remove('hidden');
-      }
-    });
+        );
+      });
+      var lightbox = new SimpleLightbox('.gallery a', {
+        /* options */
+      });
+      lightbox.refresh();
+      refs.button_load.classList.remove('hidden');
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+// function onFetch() {
+//   fetch(
+//     `${BASE_URL}/${options.key}&${options.orientation}&${options.q}&${options.safesearch}&${options.image_type}&${options.perPage}&page=${page}`
+//   )
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(obj => {
+//       items = obj;
+//       console.log('items ', items);
+//       console.log(`totalHits: ${items.totalHits}`);
+//       console.log('+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+');
+
+//       if (items.total === 0) {
+//         return notifyFail();
+//       } else {
+//         notifySuccess();
+//         items.hits.forEach(element => {
+//           refs.gallery.insertAdjacentHTML(
+//             'beforeend',
+//             `<div class="photo-card">
+//           <a href="${element.webformatURL}" >
+//     <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" /></a>
+//     <div class="info">
+//       <p class="info-item">
+//         <b>Likes <span class="box">${element.likes}</span></b>
+//       </p>
+//       <p class="info-item">
+//         <b>Views <span class="box">${element.views}</span></b>
+//       </p>
+//       <p class="info-item">
+//         <b>Comments <span class="box">${element.comments}</span></b>
+//       </p>
+//       <p class="info-item">
+//         <b>Downloads <span class="box">${element.downloads}</span></b>
+//       </p>
+//     </div>
+//   </div>`
+//           );
+//         });
+//         var lightbox = new SimpleLightbox('.gallery a', {
+//           /* options */
+//         });
+//         lightbox.refresh();
+//         refs.button_load.classList.remove('hidden');
+//       }
+//     });
+// }
 
 function notifyFail() {
   Notify.failure(
---
+++ src/test.js
@@ -0,0 +1,67 @@
+// async function onFetch() {
+//   try {
+//     const response = await axios.get(
+//       `${BASE_URL}/${options.key}&${options.orientation}&${options.q}&${options.safesearch}&${options.image_type}&${options.perPage}&page=${page}`
+//     );
+//     console.log(response);
+//     items = response.json();
+//     console.log(items);
+
+//     console.log(`totalHits: ${items.totalHits}`);
+//     console.log('+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+');
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+async function onFetch() {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/${options.key}&${options.orientation}&${options.q}&${options.safesearch}&${options.image_type}&${options.perPage}&page=${page}`
+    );
+    console.log(response.data);
+    items = response.data;
+    console.log('items ', items);
+    console.log(`totalHits: ${items.totalHits}`);
+    console.log('+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+');
+
+    console.log(`totalHits: ${items.totalHits}`);
+    console.log('+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+');
+
+    if (items.total === 0) {
+      return notifyFail();
+    } else {
+      notifySuccess();
+      items.hits.forEach(element => {
+        refs.gallery.insertAdjacentHTML(
+          'beforeend',
+          `<div class="photo-card">
+          <a href="${element.webformatURL}" >
+    <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" /></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes <span class="box">${element.likes}</span></b>
+      </p>
+      <p class="info-item">
+        <b>Views <span class="box">${element.views}</span></b>
+      </p>
+      <p class="info-item">
+        <b>Comments <span class="box">${element.comments}</span></b>
+      </p>
+      <p class="info-item">
+        <b>Downloads <span class="box">${element.downloads}</span></b>
+      </p>
+    </div>
+  </div>`
+        );
+      });
+      var lightbox = new SimpleLightbox('.gallery a', {
+        /* options */
+      });
+      lightbox.refresh();
+      refs.button_load.classList.remove('hidden');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
